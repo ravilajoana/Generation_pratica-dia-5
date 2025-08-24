7043f87fafae53780913a5adffbf1e45cb1a53d2
ravila joana <ravilajoanasousapereira@gmail.com>
2025-08-24 16:13:15 -0300

    exercicio 1 da generation brasil resolvido

--- Desktop/exercicio-pratica2/exercicio2.js
+++ Desktop/exercicio-pratica2/exercicio2.js
@@ -4,9 +4,4 @@ let nota3 = 7.0
 let nota4 = 7.5
 media = (nota1 + nota2 + nota3 + nota4) / 4 ; 
 console.log("media final: ", media);
-git init
-git branch -M main 
-git remote add origin https://github.com/ravilajoana/Generation_pratica-dia-5.git
-git add .
-git commit -m "primeiro exercicio - media notas"
-git push -u origin main 
+ 
